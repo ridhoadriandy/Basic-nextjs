e9d5551005dab062b1eaf85d13563cf7ee09ba45
unknown <ridho20ti@mahasiswa.pcr.ac.id>
2023-05-07 23:00:45 +0700

    Header selesai

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,6 +2,9 @@ import { Bars3Icon } from '@heroicons/react/24/solid';
 
 import Link from 'next/link';
 import GithubIcon from '../svgs/github';
+import InstaIcon from '../svgs/instagram';
+import TiktokIcon from '../svgs/tiktok';
+import YoutubeIcon from '../svgs/youtube';
 
 const menus = [
   { text: 'Home', href: '/' },
@@ -10,10 +13,10 @@ const menus = [
 ];
 
 const icons = [
-  { icon: GithubIcon, href: 'https://www.instagram.com/mridho_ad/' },
-  { icon: GithubIcon, href: 'https://www.instagram.com/mridho_ad/' },
-  { icon: GithubIcon, href: 'https://www.instagram.com/mridho_ad/' },
-  { icon: GithubIcon, href: 'https://www.instagram.com/mridho_ad/' },
+  { icon: GithubIcon, href: 'https://github.com/ridhoadriandy' },
+  { icon: InstaIcon, href: 'https://www.instagram.com/mridho_ad/' },
+  { icon: TiktokIcon, href: '/' },
+  { icon: YoutubeIcon, href: '/' },
 ];
 
 export default function Header() {
@@ -23,7 +26,7 @@ export default function Header() {
         <div className="py-3 md:py-4 px-4 mx-auto flex flex-row justify-between items-center md:max-w-3xl lg:max-w-7xl">
           <div className="flex place-items-center space-x-6">
             <Link href="/">
-              <div className="flex py-2 md:px-3 space-x-2 md:space-x-4 items-center">
+              <div className="flex py-2 md:px-3 space-x-2 md:space-x-4 items-center ">
                 <img
                   src="/profile.jpeg"
                   className="w-6 h-6 md:w-10 md:h-10 object-cover rounded-full"
@@ -33,9 +36,9 @@ export default function Header() {
             </Link>
 
             <nav className="hidden md:block">
-              <ul className="flex flex-row space-x-2">
+              <ul className="flex flex-row space-x-2 ">
                 {menus.map((menu, index) => (
-                  <li key={index}>
+                  <li key={index} className="hover:bg-slate-100">
                     <Link href={menu.href}>
                       <div className="px-3 py-2">{menu.text}</div>
                     </Link>
@@ -45,16 +48,20 @@ export default function Header() {
             </nav>
           </div>
 
-          <div className="flex items-center space-x-2">
-            <div className="flex">
-              {icons.map((icon, index) => (
-                <div key={index}>
-                  <icon.icon />
-                </div>
-              ))}
+          <div className="flex items-center space-x-2 lg:space-x-6">
+            <div className="hidden lg:block">
+              <div className="flex space-x-6">
+                {icons.map((icon, index) => (
+                  <div key={index} className="hover:bg-slate-100">
+                    <Link href={icon.href}>
+                      <icon.icon />
+                    </Link>
+                  </div>
+                ))}
+              </div>
             </div>
 
-            <button className="py-2 px-6 text-sm border border-red-400 rounded-full">
+            <button className="py-2 px-6 text-sm border border-red-400 rounded-full hover:bg-slate-50">
               Donwload CV
             </button>
 
